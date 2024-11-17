69ebc1256d785cc9b5410fce126a3777d4f55b11
YarZah <jarikzagreb16@gmail.com>
2024-11-17 18:02:52 +0100

    Search & sort

--- src/main/java/Sort_search/Binarysearch.java
+++ src/main/java/Sort_search/Binarysearch.java
@@ -1,4 +1,24 @@
 package Sort_search;
 
 public class Binarysearch {
+    public static int binarySearch(int[] arr, int t) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int half = left + (right - left) / 2;
+            if (arr[half] == t) {
+                return half; // Знайдено елемент
+            }
+
+            if (arr[half] > t) {
+                right = half - 1; // Шукаємо в лівій частині
+            } else {
+                left = half + 1; // Шукаємо в правій частині
+            }
+        }
+
+        return -1; // Елемент не знайдено
+    }
 }
+
--- src/main/java/Sort_search/BubbleSort.java
+++ src/main/java/Sort_search/BubbleSort.java
@@ -1,4 +1,28 @@
 package Sort_search;
 
 public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Обмін елементів
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
 
